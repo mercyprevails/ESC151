1a6ad29c8d7aed7ed7fc5d604c27e648690002e5
mercy <89814009+mercyprevails@users.noreply.github.com>
2023-02-03 10:02:10 -0500

    Update Q2.cpp

--- InClass/Quizzes/Q2.cpp
+++ InClass/Quizzes/Q2.cpp
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	double cIN = 7110, cFT, cCM;
+	cFT = (cIN / 12 / 12 / 12);
+	cCM = (cIN * 16.387064);
+	printf("volume in cubic feet: %.3lf \n", cFT);
+	printf("volume in cubic cm: %.3lf \n", cCM);
+	return(0);
+}
