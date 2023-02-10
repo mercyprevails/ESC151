014a73ab0144f00ecae44113a49fdccc43255159
mercy <89814009+mercyprevails@users.noreply.github.com>
2023-02-10 10:00:15 -0500

    Update Q3.cpp

--- InClass/Quizzes/Q3.cpp
+++ InClass/Quizzes/Q3.cpp
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415926535897932384626433832795
+int main(void)
+{
+	printf(" r            SA           V\n");
+	printf("(m)          (m^2)       (m^3)\n");
+	double V, r=0, SA;
+	for (r = 2; r <= 12; r += 2)
+	{
+		SA = 4 * PI * (r * r);
+		V = ((4 / 3) * PI) * (r * r * r);
+		printf(" %.2lf       %.2lf       %.2lf \n", r, SA, V);
+	}
+	return(0);
+}
