eb5d23a7f05d909b273f5105bfb5572b69cdc90f
mercy <89814009+mercyprevails@users.noreply.github.com>
2023-03-20 10:06:27 -0400

    Problems
    1)   	Write a user-defined function that will do the following:
       a)	Calculate the volume of a cube with side dimensions a ≤ s ≤ b.
       b)	Output the results in table form.
    
       The main function calls cube with a = 1 and b = 5.
    
    3)	Transform the computation of r into a user-defined function. The function will 
    also output the result.
    Datafile:
    10 18
    20 43
    30 57
    40 85
    50 95
    60 117
    -9 -99

--- InClass/[17] User Defined Function.cpp
+++ InClass/[17] User Defined Function.cpp
@@ -1,4 +1,4 @@
-//
+/*
 #include <stdio.h>
 int main()
 {
@@ -18,3 +18,49 @@ void cube(double a, double b)
 	}
 }
 
+*/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int c = 0, k = 0;
+	double x[20], y[20], sum_x = 0, sum_y = 0, sum_xy = 0, sum_xx = 0, sum_yy = 0, r;
+	void corr(double [], double [], int); //prototype function
+	FILE* DATA;
+	DATA = fopen("data.txt", "r");
+	if (DATA == NULL)
+		printf("\n   Error opening input file.   \n");
+	else
+	{
+		printf("\n   The input file was opened successfully.\n\n");
+		fscanf(DATA, "%lf %lf", &x[k], &y[k]);
+		do
+		{
+			printf("   x[%i] = %.0f, y[%i] = %.0f \n", k, x[k], k, y[k]);
+			c++;
+			k++;
+			fscanf(DATA, "%lf %lf", &x[k], &y[k]);
+		} while (x[k] >= 0);
+		printf("\n   c = %i \n", c);
+	}
+	corr(x, y, c);
+	printf("\n\n");
+	return 0;
+}
+
+void corr( double x[20], double y[20], int c)
+{
+	int k;
+	double r, sum_x = 0, sum_y = 0, sum_xy = 0, sum_xx = 0, sum_yy = 0;
+	for (k = 0; k <= c - 1; k++)
+	{
+		sum_x += x[k];
+		sum_y += y[k];
+		sum_xx += x[k] * x[k];
+		sum_yy += y[k] * y[k];
+		sum_xy += x[k] * y[k];
+	}
+	r = (c * sum_xy - sum_x * sum_y) / sqrt((c * sum_xx - sum_x * sum_x) * (c * sum_yy - sum_y * sum_y));
+	printf("\n\n   r = %.4f \n", r);
+}
