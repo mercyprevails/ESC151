6ec9ec7cf81165bc7443fa71c8d729eaee02225f
mercy <89814009+mercyprevails@users.noreply.github.com>
2023-03-20 09:47:23 -0400

    Create [17] User Defined Function.cpp

---
+++ InClass/[17] User Defined Function.cpp
@@ -0,0 +1,19 @@
+//
+#include <stdio.h>
+int main()
+{
+	double a = 1, b = 5; //declarations
+	void cube(double, double); //prototype function
+	cube(a, b); // assigning variables to above function (function call)
+	return 0;
+}
+
+void cube(double a, double b)
+{
+	double s;
+	printf("\n   Side Dim.   Volume \n\n");
+	for (s = a; s <= b; s++)
+	{
+		printf("  %5.0f      %5.0f\n", s, s * s * s);
+	}
+}
